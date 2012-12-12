7e9ce462046c331e52a7685b20dec5e2d758fd0f
Zaicheng Qi <vmlinz@gmail.com>
2012-12-13 01:07:31 +0800

    * set original board name and prompt

--- include/configs/tiny210.h
+++ include/configs/tiny210.h
@@ -131,7 +131,7 @@
 #define CONFIG_SYS_LONGHELP             /* undef to save memory */
 #define CONFIG_SYS_HUSH_PARSER          /* use "hush" command parser    */
 #define CONFIG_SYS_PROMPT_HUSH_PS2      "> "
-#define CONFIG_SYS_PROMPT              "[ZZ-TINY210]# "
+#define CONFIG_SYS_PROMPT              "[UBOOT-TINY210]# "
 #define CONFIG_SYS_CBSIZE               256     /* Console I/O Buffer Size*/
 #define CONFIG_SYS_PBSIZE               384     /* Print Buffer Size */
 #define CONFIG_SYS_MAXARGS              16      /* max number of command args */
